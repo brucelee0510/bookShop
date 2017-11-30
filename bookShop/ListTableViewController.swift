f753974ab828ba08d7786aacbfc4613404e830dd
BruceLee <brucelee@BruceLeedeMacBook-Pro.local>
2017-11-30 16:53:00 +0800

    commit

---
+++ bookShop/ListTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ListTableViewController.swift
+//  bookShop
+//
+//  Created by BruceLee on 2017/11/30.
+//  Copyright © 2017年 BruceLee. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+    var books = [Book]()
+    @IBOutlet var BookNameLabels: [UILabel]!
+    @IBOutlet var BookCoverImages: [UIImageView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        books.append(Book(name:"人人都能學會製作厲害圖表", author:"Smart智富", imgName:"book1"))
+        books.append(Book(name:"Google6大神器‧提升工作效率100%", author:"Smart智富", imgName:"book2"))
+        for i in 0..<BookNameLabels.count{
+            BookNameLabels[i].text = books[i].name
+            BookCoverImages[i].image = UIImage(named: books[i].imgName)
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        let button = sender as? UIButton
+        let ContentViewController = segue.destination as? ContentViewController
+        ContentViewController?.name = button?.currentTitle
+    }
+ 
+
+}
---
+++ bookShop/book.swift
@@ -0,0 +1,15 @@
+//
+//  book.swift
+//  bookShop
+//
+//  Created by BruceLee on 2017/11/30.
+//  Copyright © 2017年 BruceLee. All rights reserved.
+//
+
+import Foundation
+
+struct Book{
+    var name: String
+    var author: String
+    var imgName: String
+}
